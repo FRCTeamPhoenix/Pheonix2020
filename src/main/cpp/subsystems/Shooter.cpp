dee78806b964e521db1447f08d6fdd746725cf1a
Christian Baduria <christian.baduria@gmail.com>
2020-01-27 21:16:40 -0500

    Worked on the grabber, loader, and shooter

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -7,7 +7,7 @@
 
 Shooter::Shooter() {}
 
-void Shooter:initialize() {
+void Shooter::initialize() {
     // Configure encoders on motors (remove if encoder is absent)
     m_shooterTop.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, TIMEOUT);
     m_shooterBottom.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0, TIMEOUT);
@@ -24,38 +24,65 @@ void Shooter:initialize() {
     m_loaderRight.SetInverted(true);
 
     // 
-    m_shooterTop.ConfigNominalOutputForward(0, TIMEOUT);
-    m_shooterTop.ConfigNominalOutputReverse(0, TIMEOUT);
-    m_shooterBottom.ConfigNominalOutputForward(0, TIMEOUT);
-    m_shooterBottom.ConfigNominalOutputReverse(0, TIMEOUT);
-    m_loaderLeft.ConfigNominalOutputForward(0, TIMEOUT);
-    m_loaderLeft.ConfigNominalOutputReverse(0, TIMEOUT);
-    m_loaderRight.ConfigNominalOutputForward(0, TIMEOUT);
-    m_loaderLeft.ConfigNominalOutputReverse(0, TIMEOUT);
-    m_grabber.ConfigNominalOutputForward(0, TIMEOUT);
-    m_grabber.ConfigNominalOutputReverse(0, TIMEOUT);
+    m_shooterTop.ConfigNominalOutputForward(0);
+    m_shooterTop.ConfigNominalOutputReverse(0);
+    m_shooterBottom.ConfigNominalOutputForward(0);
+    m_shooterBottom.ConfigNominalOutputReverse(0);
+    m_loaderLeft.ConfigNominalOutputForward(0);
+    m_loaderLeft.ConfigNominalOutputReverse(0);
+    m_loaderRight.ConfigNominalOutputForward(0);
+    m_loaderRight.ConfigNominalOutputReverse(0);
+    m_grabber.ConfigNominalOutputForward(0);
+    m_grabber.ConfigNominalOutputReverse(0);
 
-    // Configure power limits imposed on motors (change if needed)
-    m_shooterTop.ConfigPeakOutputForward(1.0, TIMEOUT);
-    m_shooterTop.ConfigPeakOutputReverse(-1.0, TIMEOUT);
-    m_shooterBottom.ConfigPeakOutputForward(1.0, TIMEOUT);
-    m_shooterBottom.ConfigPeakOutputReverse(-1.0, TIMEOUT);
-    m_loaderLeft.ConfigPeakOutputForward(1.0, TIMEOUT);
-    m_loaderLeft.ConfigPeakOutputReverse(-1.0, TIMEOUT);
-    m_loaderRight.ConfigPeakOutputForward(1.0, TIMEOUT);
-    m_loaderRight.ConfigPeakOutputReverse(-1.0, TIMEOUT);
+    // Configure power limits on motors (change if needed)
+    m_shooterTop.ConfigPeakOutputForward(1.0);
+    m_shooterTop.ConfigPeakOutputReverse(-1.0);
+    m_shooterBottom.ConfigPeakOutputForward(1.0);
+    m_shooterBottom.ConfigPeakOutputReverse(-1.0);
+    m_loaderLeft.ConfigPeakOutputForward(1.0);
+    m_loaderLeft.ConfigPeakOutputReverse(-1.0);
+    m_loaderRight.ConfigPeakOutputForward(1.0);
+    m_loaderRight.ConfigPeakOutputReverse(-1.0);
+    m_grabber.ConfigPeakOutputForward(1.0);
+    m_grabber.ConfigPeakOutputReverse(-1.0);
+    
+    m_shooterTop.Follow(m_shooterBottom);
+    m_loaderLeft.Follow(m_loaderRight);
 }
 
-void Shooter::setShooterSpeed(const double& top, const double& bottom) {
-    m_shooterTop.Set(ControlMode::Velocity, top);
-    m_shooterBottom.Set(ControlMode::Velocity, bottom);
+void Shooter::setShooterSpeed(const double& speed) {
+    m_shooterTop.Set(ControlMode::Velocity, speed);
+    m_shooterBottom.Set(ControlMode::Velocity, speed);
 }
 
-void Shooter::setLoaderSpeed(const double& left, const double& right) {
-    m_loaderLeft.Set(ControlMode::MotionMagic, left);
-    m_loaderRight.Set(ControlMode::MotionMagic, right);
+void Shooter::setLoaderSpeed(const double& speed) {
+    m_loaderLeft.Set(ControlMode::MotionMagic, speed);
+    m_loaderRight.Set(ControlMode::MotionMagic, speed);
 }
 
-void Shooter::setGrabberSpeed(const double& front) {
-    m_grabber.Set(ControlMode::PercentOutput, front);
+void Shooter::setGrabberSpeed(const double& speed) {
+    m_grabber.Set(ControlMode::PercentOutput, speed);
+}
+
+void Shooter::grab() {
+    setGrabberSpeed(0.5);
+}
+
+void Shooter::reload() {
+    setLoaderSpeed(0.1);
+}
+
+void Shooter::shootLow() {
+    setShooterSpeed(0.5);
+}
+
+void Shooter::shootHigh() {
+    setShooterSpeed(1.0);
+}
+
+void Shooter::stop() {
+    setShooterSpeed(0);
+    setLoaderSpeed(0);
+    setGrabberSpeed(0);
 }
--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -15,14 +15,18 @@ class Shooter : public frc2::SubsystemBase {
         Shooter(Shooter const&) = delete;
         void operator = (Shooter const&) = delete;
         void initialize();
-        void setShooterSpeed(const double& top, const double& bottom);
-        void setLoaderSpeed(const double& left, const double& right);
-        void setGrabberSpeed(const double& front);
-        void end();
+        void setShooterSpeed(const double& speed);
+        void setLoaderSpeed(const double& speed);
+        void setGrabberSpeed(const double& speed);
+        void grab();
+        void reload();
+        void shootLow();
+        void shootHigh();
+        void stop();
     
     private:
         Shooter();
-        const int TIMEOUT = 10;
+        // const int TIMEOUT = 10;
         TalonSRX m_shooterTop;
         TalonSRX m_shooterBottom;
         TalonSRX m_loaderLeft;
