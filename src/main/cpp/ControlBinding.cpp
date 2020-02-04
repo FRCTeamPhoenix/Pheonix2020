7e26571b51a35c81074b1c94f038783ff3f0b3e4
Christian Baduria <christian.baduria@gmail.com>
2020-02-03 21:17:52 -0500

    Used HashMaps to store Control Data

--- src/main/cpp/ControlBinding.cpp
+++ src/main/cpp/ControlBinding.cpp
@@ -1,2 +1,24 @@
 #include "ControlBinding.h"
 
+ControlBinding::ControlBinding() {}
+
+void ControlBinding::initialize() {
+    m_controlData["driveLeft"] = {ControlType::AXIS, 0};
+    m_controlData["driveRight"] = {ControlType::AXIS, 1};
+    m_controlData["shift"] = {ControlType::BUTTON, 2};
+    m_controlData["shoot"] = {ControlType::BUTTON, 3};
+    m_controlData["intake"] = {ControlType::BUTTON, 4};
+    m_controlData["tiltIntake"] = {ControlType::AXIS, 5};
+}
+
+bool ControlBinding::getControlValue(std::string control) {
+    auto it = m_controlData.find(control);
+
+    // Check if control exists, then get control value
+    if (it != m_controlData.end()) {
+        // it->second.id;
+        // it->second.type;
+    } else {
+        // Throw programmer-defined ControlDoesNotExistException
+    }
+}
--- src/main/include/ControlBinding.h
+++ src/main/include/ControlBinding.h
@@ -1,25 +1,30 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
 #include <frc/Joystick.h>
 #include <frc2/command/button/JoystickButton.h>
 
 #include "Constants.h"
 
+enum class ControlType {
+    AXIS, BUTTON
+};
+
+struct ControlData {
+    ControlType type;
+    int id;
+};
+
 class ControlBinding {
     public:
+        ControlBinding();
         void initialize();
-        void execute();
-        void end();
-        // Driver controls
-        bool shiftGearButtonIsPressed();
-        bool leftDriveAxisIsActive();
-        bool rightDriveAxisIsActive();
-        // Operator controls
-        bool shooterButtonIsPressed();
-        bool intakeButtonIsPressed();
-        bool intakeTiltAxisIsActive();
+        bool getControlValue(std::string control);
 
     private:
         frc::Joystick m_driverJoystick{DRIVER_CONTROL};
         frc::Joystick m_operatorJoystick{OPERATOR_CONTROL};
-}
+        std::unordered_map<std::string, ControlData> m_controlData;
+};
